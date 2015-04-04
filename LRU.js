21081a9b933f372a113575cb11bd2bff9c41825d
ramejones <mrhb1993@hotmail.com>
2015-04-04 23:25:57 +0500

    Second Version of program
    LRU logic is implemented but not working properly.
    debugging required.

--- LRU.js
+++ LRU.js
@@ -3,9 +3,12 @@
  */
 var getPageTableFrames;
 var getPhysicalFramesNumber;
-var frameStack=[];
-var referenceStringArray=[7,7,0,1];
-//var range=[0,1,2,3,4,5,6,7];
+var framesArray=[];
+var referenceStringArray=[7,7,0,1,2];
+var lruStack=[];
+var temp4ReferenceRepeat=0;
+var tempCnt=1;
+
 
 function getPageFrames(){
     getPageTableFrames=parseInt(document.getElementById("pageFrameNumber").value);
@@ -18,8 +21,36 @@ function getPhysicalFrames(){
 
 function initializeFrameStack(pageTableFrames){
     for(var count=0;count<pageTableFrames;count++){
-        frameStack[count]=-1;
+        framesArray[count]=-1;
     }
+}
+
+function loopingFramesArray(){
+
+    for(var temp=0;temp<framesArray.length;temp++){
+        if(framesArray[temp]!=1){
+            tempCnt++;
+        }
+    }
+    if(tempCnt==framesArray.length)
+        return true;
+    else
+        return false;
+}
+
+function checkReferenceRepeat(currentReferenceCounter){
+
+    for(var counter=currentReferenceCounter;counter>0;counter--){
+        if(referenceStringArray[currentReferenceCounter]==referenceStringArray[counter-1]){
+            temp4ReferenceRepeat++;
+        }
+    }
+
+    if(temp4ReferenceRepeat==currentReferenceCounter){
+        return true;}
+    else{
+        return false;}
+
 }
 
 function generateRandomReferenceString(){
@@ -28,7 +59,7 @@ function generateRandomReferenceString(){
 //    for(var count=0;count<getPhysicalFramesNumber;count++){
 //        referenceStringArray[count]=Math.floor((Math.random()*8));
 //    }
-    console.log(frameStack);
+    console.log(framesArray);
     console.log(referenceStringArray);
 }
 
@@ -36,22 +67,39 @@ function generateLRU(){
     generateRandomReferenceString();
     var currentPageFrame,secondCounter,thirdCounter;
     for(currentPageFrame=0;currentPageFrame<referenceStringArray.length;currentPageFrame++){
-        loopingFrameStack:{
-            for(secondCounter=0;secondCounter<frameStack.length;secondCounter++){
-                if(frameStack[secondCounter]==-1){
+        loopingFrameArray:{
+            for(secondCounter=0;secondCounter<framesArray.length;secondCounter++){
+                if(framesArray[secondCounter]==-1){
                     for(thirdCounter=currentPageFrame;thirdCounter>0;thirdCounter--){
                         if(referenceStringArray[currentPageFrame]==referenceStringArray[thirdCounter-1]){
-                            break loopingFrameStack;
+                            break loopingFrameArray;
                         }
                     }
-                    frameStack[secondCounter]=referenceStringArray[currentPageFrame];
-                    break loopingFrameStack;
+                    framesArray[secondCounter]=referenceStringArray[currentPageFrame];
+                    lruStack.unshift(referenceStringArray[currentPageFrame]);
+                    break loopingFrameArray;
                 }
             }
-
         }
-//        console.log(referenceStringArray);
-        console.log("FrameStack"+frameStack);
+        AgainLoopingFrameArray:{
+            if(loopingFramesArray()){
+                if(checkReferenceRepeat(currentPageFrame)){
+                    lruStack.pop();
+                    lruStack.unshift(referenceStringArray[currentPageFrame]);
+                    for(var finderReferenceString=0;finderReferenceString<framesArray.length;finderReferenceString++){
+                        if(framesArray[finderReferenceString]==lruStack[0]){
+                            finderReferenceString[finderReferenceString]=lruStack[0];
+                        }
+                    }
+
+                }
+                else{
+                    break AgainLoopingFrameArray;
+                }
+            }
+        }
+        console.log(lruStack);
+        console.log(framesArray);
     }
 
 }
